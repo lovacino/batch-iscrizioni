3ea8b9f45f832f90f2cff57dda82ecc33f43040f
AS <antonio.salvati@fides.it>
2022-02-11 15:45:05 +0100

    Fix in caso di data assente prendo la data odierna

--- src/main/java/it/lovacino/iltrotto/App.java
+++ src/main/java/it/lovacino/iltrotto/App.java
@@ -4,6 +4,8 @@ import it.lovacino.iltrotto.service.SorteggiService;
 
 import org.apache.log4j.Logger;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -15,17 +17,14 @@ public class App
 {
 
     private static final Logger logger = Logger.getRootLogger();
-//    private static final Logger logger = Logger.getLogger(App.class);
-    
-    
 
     public static void main( String[] args )
     {
         try {
             logger.info("[main] - start batch at " + LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME) + " - [START] --- [INPUT]: " + args );
-            String dataCorsa = String.valueOf(args[1]);
-            SorteggiService sorteggioService = SorteggiService.getInstance();
-            sorteggioService.batchSorteggi(dataCorsa);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            String dataCorsa = args!=null && args.length>0 ? String.valueOf(args[0]) : formatter.format(LocalDate.now());;
+            SorteggiService.getInstance().batchSorteggi(dataCorsa);
             logger.info("[main] - end batch at " + LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME) + " - [END OK] --- [NO RETURN]");
         }catch (Exception e){
             logger.error("[main] - error batch at " + LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME) + " - [END KO] --- [ERROR]: ", e);
